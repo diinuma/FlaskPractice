ee91907b4734fe6f31580fc4645fe285b42e5885
iinuma-winc <iinuma@winc.co.jp>
2020-12-04 10:41:28 +0900

    ファイルを整理

--- app/main.py
+++ app/main.py
@@ -103,30 +103,6 @@ def delete(id=None):
 
     return redirect(url_for('todo_list'))
 
-"""
-@app.route('/')
-def hello():
-    name = "Hello World!!"
-    with pg.connect(**DATABASE) as conn, conn.cursor() as cur:
-        sql = """
-            #select * 
-            #from todo
-            #where todo = %(todo)s
-"""
-        params = {
-            'todo':'flaskapp'
-        }
-        cur.execute(sql, params)
-        rows = cur.fetchall()
-
-    return render_template('hello.html', title='flask test', name=name, rows=rows)
-
-@app.route('/good')
-def good():
-    name = "Good"
-    return render_template('good.html', title='flask test', val1="Good", val2="Bad")
-"""
-
 # サーバーを実行する
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
