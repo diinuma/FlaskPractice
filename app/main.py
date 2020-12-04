09570e5b49a8db85d9e6537fdf6d392f6cf17093
iinuma-winc <iinuma@winc.co.jp>
2020-12-04 09:39:45 +0900

    登録画面を追加

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import psycopg2 as pg
+import psycopg2.extras
 
 app = Flask(__name__)
 
+# DB接続情報
 DATABASE = {
     'host':'db',
     'dbname':'flaskdb',
@@ -12,14 +14,37 @@ DATABASE = {
 }
 
 @app.route('/')
+def todo_list():
+    with pg.connect(**DATABASE) as connection, \
+         connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        sql = """
+            select * from todo
+        """
+
+        cursor.execute(sql)
+        todos = cursor.fetchall()
+
+    return render_template('list.html', title='TODOアプリ', todos=todos)
+
+
+@app.route('/add')
+def todo_add():
+    return render_template('add.html', title='TODOアプリ')
+
+@app.route('/', methods=['POST'])
+def add():
+    return redirect(url_for('todo_list'))
+
+"""
+@app.route('/')
 def hello():
     name = "Hello World!!"
     with pg.connect(**DATABASE) as conn, conn.cursor() as cur:
         sql = """
-            select * 
-            from todo
-            where todo = %(todo)s
-        """
+            #select * 
+            #from todo
+            #where todo = %(todo)s
+"""
         params = {
             'todo':'flaskapp'
         }
@@ -32,6 +57,7 @@ def hello():
 def good():
     name = "Good"
     return render_template('good.html', title='flask test', val1="Good", val2="Bad")
+"""
 
 # サーバーを実行する
 if __name__ == "__main__":
